31269a4b4e4009ad09dc2c3c6b1a1037d3b31bd6
Loren Gordon <lorengordon@users.noreply.github.com>
2023-09-06 07:31:34 -0700

    Adds tests for supported use cases

---
+++ tests/test-policy-with-attachments/main.tf
@@ -0,0 +1,36 @@
+module "policy" {
+  source = "../.."
+
+  policy = {
+    name    = "tardigrade-test-policy-${local.test_id}"
+    content = data.aws_iam_policy_document.admin.json
+
+    attachments = [
+      {
+        name      = "tardigrade-test-policy-attachment-${local.test_id}"
+        target_id = data.aws_organizations_organization.this.roots[0].id
+      }
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "admin" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_organizations_organization" "this" {}
+
+data "terraform_remote_state" "prereq" {
+  backend = "local"
+  config = {
+    path = "prereq/terraform.tfstate"
+  }
+}
+
+locals {
+  test_id = data.terraform_remote_state.prereq.outputs.random_string.result
+}
